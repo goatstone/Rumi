38691dc1cb1027f2eaacd86e32d733d8d92cf09c
Jose Herminio Collas <joseherminiocollas@goatstone.com>
2026-06-25 10:09:50 -0500

    Update tools to test for .ts

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,12 @@
 export default {
+  preset: "ts-jest/presets/default-esm", // ESM + TS
+  testEnvironment: "node",
+  extensionsToTreatAsEsm: [".ts"],
+  globals: {
+    "ts-jest": {
+      useESM: true,
+    },
+  },
   testEnvironment: "node",
   transform: {
     "^.+\\.jsx?$": "babel-jest"
